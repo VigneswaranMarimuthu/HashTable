5e8acd18c60f15a5195004005d1adeb8bcf8009e
Vigneswaran <vignes.marimuthu@gmail.com>
2013-03-14 22:23:28 +0530

    Create HashTable.js
    Implemented hash table in javascript. ( I am still learning javascript. So, if any error feel free to intimate me :) )

---
+++ HashTable.js
@@ -0,0 +1,202 @@
+/**
+ * HashTable Class.
+ * 
+ * @author Vigneswaran Marimuthu
+ * 
+ * @returns HashTable
+ */
+
+function HashTable(){
+  
+	if(!(this instanceof arguments.callee))
+		return new arguments.callee();
+	
+	/**
+	 * Private Variables
+	 */
+	var length = 0;
+	var collection = new Collection();
+	
+	/**
+	 * Returns the size of the collection.
+	 * 
+	 * @author Vigneswaran Marimuthu
+	 * 
+	 * @returns Number
+	 */
+	this.getSize = function(){
+		return length;
+	};
+	
+	/**
+	 * Adds the value to the collection with the specified key. If the key already exists,
+	 * its value in the collections is replaced with the new value. Returns <code>null</code>,
+	 * if passed key or value is invalid, or the length of the collection.
+	 * 
+	 * @author Vigneswaran Marimuthu
+	 * 
+	 * @param String
+	 * @param String / <b>Boolean</b> / <b>Number</b> / <b>Object</b> / <b>Array</b>
+	 * 
+	 * @returns Number or <code>null</code>
+	 */
+	this.add = function(key, value){
+		if((isString(key) && key.trim() !== '') && (value !== null && value !== undefined && !isFunction(value))){
+			if(this.hasKey(key)){
+				collection[key] = value;
+				return length;
+			}
+			collection[key] = value;
+			return ++length;
+		}
+		return null;
+	};
+	
+	/**
+	 * Adds all the properties of the passed object including its constructor's prototype.
+	 * Specifying the second parameter to <code>true</code> will neglects the addition of
+	 * constructor's prototype properties. Returns <code>null</code>, if the passed object 
+	 * is invalid, or the length of the collection.
+	 * 
+	 * @author Vigneswaran Marimuthu
+	 * 
+	 * @param Object
+	 * @param Boolean
+	 * 
+	 * @returns Number or <code>null</code>
+	 */
+	this.addAll = function(collections, useOwnProp){
+		if(isObject(collections)){
+			
+			if(collections instanceof this.constructor)
+				collections = collections.getAll();
+			
+			for(var prop in collections){
+				if(useOwnProp === true){
+					if(collections.hasOwnProperty(prop))
+						this.add(prop, collections[prop]);
+				}
+				else
+					this.add(prop, collections[prop]);
+			}
+			return length;
+		}
+		return null;
+	};
+	
+	/**
+	 * Checks for the passed key in the collection. Returns <code>true</code>, if the key is
+	 * present, or <code>false</code>, if the key is not present, or <code>null</code>, if
+	 * the passed key is invalid.
+	 * 
+	 * @author Vigneswaran Marimuthu
+	 * 
+	 * @param String
+	 * 
+	 * @returns Boolean or <code>null</code>
+	 */
+	this.hasKey = function(key){
+		return (isString(key)) ? collection.hasOwnProperty(key) : null;
+	};
+	
+	/**
+	 * Returns the value for the passed key stored in the collection or <code>undefined</code>,
+	 * if the passed key is not present in the collection, or <code>null</code>, if the passed
+	 * key is invalid.
+	 * 
+	 * @author Vigneswaran Marimuthu
+	 * 
+	 * @param String
+	 * 
+	 * @returns String or Boolean or Number or Object or Array or <code>null</code>
+	 */
+	this.get = function(key){
+		return (isString(key) && key.trim() !== '') ? collection[key] : null;
+	};
+	
+	/**
+	 * Returns the associated collection.
+	 * 
+	 * @author Vigneswaran Marimuthu
+	 * 
+	 * @returns Collection
+	 */
+	this.getAll = function(){
+		return collection;
+	};
+	
+	/**
+	 * Removes the key from the collection. Returns <code>true</code>, if the key is deleted,
+	 * or <code>false</code>, if the key is not present, or <code>null</code>, if the key is
+	 * invalid.
+	 * 
+	 * @author Vigneswaran Marimuthu
+	 * 
+	 * @param String
+	 * 
+	 * @returns Number or <code>null</code>
+	 */
+	this.remove = function(key){
+		if(isString(key) && key.trim() !== ''){
+			if(this.hasKey(key)){
+				length--;
+				return delete collection[key];
+			}
+			return false;
+		}
+		return null;
+	};
+	
+	/**
+	 * Removes all data in the collection.
+	 * 
+	 * @author Vigneswaran Marimuthu
+	 * 
+	 * @returns Number
+	 */
+	this.removeAll = function(){
+		collection = new collection.constructor();
+		return length = 0;
+	};
+	
+
+	/* Private */
+	
+	/**
+	 * Collection Class
+	 * 
+	 * @author Vigneswaran Marimuthu
+	 * 
+	 * @type Private 
+	 */
+	function Collection(){}
+	
+	/**
+	 * Private Utility Methods
+	 */
+	
+	function isFunction(value){
+		return Object.prototype.toString.call(value) === "[object Function]";
+	}
+	function isNumber(value){
+		return Object.prototype.toString.call(value) === "[object Number]";
+	}
+	function isBoolean(value){
+		return Object.prototype.toString.call(value) === "[object Boolean]";
+	}
+	function isString(value){
+		return Object.prototype.toString.call(value) === "[object String]";
+	}
+	function isObject(value){
+		return Object.prototype.toString.call(value) === "[object Object]";
+	}
+	function isArray(value){
+		return Object.prototype.toString.call(value) === "[object Array]";
+	}
+	function isNull(value){
+		return Object.prototype.toString.call(value) === "[object Null]";
+	}
+	function isUndefined(value){
+		return Object.prototype.toString.call(value) === "[object Undefined]";
+	}
+}
